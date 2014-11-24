79ace77e751313f5537987502eda44199d3a3d44
Lars Gollnow <misc@mfind.de>
2014-11-24 14:16:13 +0100

    * moved NodeMock

---
+++ spec/support/node_mock.rb
@@ -0,0 +1,28 @@
+class NodeMock
+  def labels
+    [:Item]
+  end
+
+  def neo_id
+    22
+  end
+
+  def props
+    { app:      'sause',
+      title:    'Kerze',
+      ident:    'item-44',
+      uuid:     'xyz',
+
+      image_url: 'http://image.url',
+      created_at: DateTime.parse('2014-11-24 13:11:22').to_s,
+      updated_at: DateTime.parse('2014-11-24 13:11:22').to_s,
+
+      height:   35,
+      weight:   240,
+      color:    'rot' }
+  end
+
+  # def method_missing(method, *args)
+  #   props[method] if props.has_key?(method)
+  # end
+end
