7609f399751af49806512f18a97952892c7156a7
Lars Gollnow <misc@mfind.de>
2014-12-03 14:48:29 +0100

    * changed repos source

--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-set :branch, 'master'
-set :rack_env, 'production'
-set :rails_env, 'production'
-
-set :repo_url, 'git@github.com:jhmegorei/dinghub.git'
-
-server '134.119.24.49', user: 'live', port: 22, roles: %w{web app db}
