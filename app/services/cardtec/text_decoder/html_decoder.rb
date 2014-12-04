dd88eed154cbe3703604d1ba4a9b6dbd82e08a9f
Johannes Haubold <jh@megorei.com>
2014-12-04 12:06:17 +0100

    small fix

--- app/services/cardtec/text_decoder/html_decoder.rb
+++ app/services/cardtec/text_decoder/html_decoder.rb
@@ -6,7 +6,7 @@ class Cardtec::TextDecoder::HtmlDecoder
 
   def to_hash
     props = {}
-    Nokogiri::HTML.fragment(data).css('.cardtec_property').each do |property|
+    Nokogiri::HTML.fragment(@data).css('.cardtec_property').each do |property|
       property_name = property.css('.cardtec_property_name').first.text.presence
       property_value = property.css('.cardtec_property_value').first.text.presence
       props[property_name] = property_value if property_name
