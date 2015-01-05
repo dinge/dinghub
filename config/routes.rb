d5b908ae463220ed435a0fe5cf9e392b8566ae5c
Lars Gollnow <misc@mfind.de>
2015-01-05 16:14:05 +0100

    * using concerns in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,31 @@
 Rails.application.routes.draw do
   root 'maker/concepts#index'
 
+  concern :filterable do
+    get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
+  end
+
   namespace :raw_node do
     root 'nodes#index'
-    resources :nodes do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
+    resources :nodes, concerns: [:filterable]
   end
 
 
   namespace :me do
     root 'actors#index'
-    resources :actors do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
-    resources :contributions do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
+    resources :actors, concerns: [:filterable]
+    resources :contributions, concerns: [:filterable]
   end
 
   namespace :maker do
     root 'concepts#index'
-    resources :topics do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
-    resources :concepts do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
-    resources :traits do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
-    resources :skills do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
-    resources :bundles do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
-    resources :items do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
-    resources :trait_values do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
+    resources :topics, concerns: [:filterable]
+    resources :concepts, concerns: [:filterable]
+    resources :traits, concerns: [:filterable]
+    resources :skills, concerns: [:filterable]
+    resources :bundles, concerns: [:filterable]
+    resources :items, concerns: [:filterable]
+    resources :trait_values, concerns: [:filterable]
 
     resources :relationships do
       match 'between/:first_node/:second_node', action: :between, on: :collection, as: :between_nodes, via: [:get, :post]
@@ -52,9 +36,7 @@ Rails.application.routes.draw do
 
   namespace :blog do
     root 'posts#index'
-    resources :posts do
-      get 'filtered/:filter', on: :collection, action: :filtered, as: :filtered
-    end
+    resources :posts, concerns: [:filterable]
   end
 
 
