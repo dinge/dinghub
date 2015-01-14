6c4f17ee2e49eca6d8732ef1c66138765908488e
Johannes Haubold <jh@megorei.com>
2015-01-14 15:59:57 +0100

    link secrets.yml

--- config/deploy/banana.rb
+++ config/deploy/banana.rb
@@ -3,6 +3,6 @@ set :rack_env, 'production'
 set :rails_env, 'production'
 
 set :repo_url, 'git@github.com:jhmegorei/dinghub.git'
-set :linked_files, ["config/database.yml", "config/thin/banana.yml"]
+set :linked_files, ["config/secrets.yml", "config/database.yml", "config/thin/banana.yml"]
 
 server '192.168.178.108', user: 'live', port: 22, roles: %w{web app db}
