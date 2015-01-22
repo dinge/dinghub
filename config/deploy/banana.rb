2347fb2f9292edcff9a5da29457aacaa4f72a14f
Johannes Haubold <jh@megorei.com>
2015-01-22 15:56:18 +0100

    reverted deploy files to original dinge repo

--- config/deploy/banana.rb
+++ config/deploy/banana.rb
@@ -5,7 +5,7 @@ set :branch, 'master'
 set :rack_env, 'production'
 set :rails_env, 'production'
 
-set :repo_url, 'git@github.com:jhmegorei/dinghub.git'
+set :repo_url, 'git@github.com:dinge/dinghub.git'
 set :linked_files, ["config/secrets.yml", "config/database.yml", "config/thin/banana.yml"]
 
 server '192.168.178.108', user: 'live', port: 22, roles: %w{web app db}
